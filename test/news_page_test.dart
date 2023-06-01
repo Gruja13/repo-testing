c561bcf61808ee7ba9d0eb5fefb69b4c0833b25a
Djordje Grujovic <djgrujovic13@gmail.com>
2023-06-01 09:17:49 +0200

    Adding test widgets

--- test/news_page_test.dart
+++ test/news_page_test.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
 import 'package:testing_flutter/article.dart';
+import 'package:testing_flutter/news_change_notifier.dart';
+import 'package:testing_flutter/news_page.dart';
 import 'package:testing_flutter/news_service.dart';
 
 class MockNewsService extends Mock implements NewsService {}
@@ -28,4 +32,45 @@ void main() {
       return articlesFromService;
     });
   }
+
+  Widget createWidgetUnderTest() {
+    return MaterialApp(
+      title: 'News app',
+      home: ChangeNotifierProvider(
+        create: (_) => NewsChangeNotifier(mockNewsService),
+        child: const NewsPage(),
+      ),
+    );
+  }
+
+  testWidgets('title is displayed', (WidgetTester widgetTester) async {
+    arrangeNewsServiceReturns3Articles();
+    await widgetTester.pumpWidget(createWidgetUnderTest());
+    expect(find.text('News'), findsOneWidget);
+  });
+
+  testWidgets('loading indicator is displayed while waiting for articles',
+      (WidgetTester widgetTester) async {
+    arrangeNewsServiceReturns3ArticlesAfter2SecondWait();
+
+    await widgetTester.pumpWidget(createWidgetUnderTest());
+    await widgetTester.pump(const Duration(milliseconds: 500));
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    await widgetTester.pumpAndSettle();
+  });
+
+  testWidgets('articles are displayed', (WidgetTester widgetTester) async {
+    arrangeNewsServiceReturns3Articles();
+
+    await widgetTester.pumpWidget(createWidgetUnderTest());
+
+    await widgetTester.pump();
+
+    for (final article in articlesFromService) {
+      expect(find.text(article.title), findsOneWidget);
+      expect(find.text(article.content), findsOneWidget);
+    }
+  });
 }
